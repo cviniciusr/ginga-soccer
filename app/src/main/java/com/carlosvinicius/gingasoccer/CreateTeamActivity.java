462a2cb43437b5fcd9d25257ef82433a444a6abb
carlos.oliveira <carlos.oliveira@infobase.com.br>
2019-03-28 16:55:41 -0300

    Adição de DatePicker e TimePicker

--- app/src/main/java/com/carlosvinicius/gingasoccer/CreateTeamActivity.java
+++ app/src/main/java/com/carlosvinicius/gingasoccer/CreateTeamActivity.java
@@ -1,5 +1,6 @@
 package com.carlosvinicius.gingasoccer;
 
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +10,7 @@ import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.carlosvinicius.gingasoccer.models.Team;
 import com.carlosvinicius.gingasoccer.models.UsersTeam;
@@ -17,10 +19,12 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 import java.util.Arrays;
+import java.util.Calendar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import butterknife.OnTouch;
 
 public class CreateTeamActivity extends AppCompatActivity {
 
@@ -69,6 +73,51 @@ public class CreateTeamActivity extends AppCompatActivity {
                 userKey = intent.getStringExtra(getResources().getString(R.string.user_key));
             }
         }
+
+        mStartTimeEditText.setOnClickListener(view -> setTime(mStartTimeEditText));
+        mEndTimeEditText.setOnClickListener(view -> setTime(mEndTimeEditText));
+
+//        mStartTimeEditText.setOnTouchListener(view -> {
+//            Calendar mcurrentTime = Calendar.getInstance();
+//            int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+//            int minute = mcurrentTime.get(Calendar.MINUTE);
+//            TimePickerDialog mTimePicker;
+//            mTimePicker = new TimePickerDialog(CreateTeamActivity.this,
+//                    (timePicker, selectedHour, selectedMinute) -> {
+//                        mStartTimeEditText.setText( selectedHour + ":" + selectedMinute);
+//                    },
+//                    hour,
+//                    minute,
+//                    true);
+//            mTimePicker.setTitle("Select Time");
+//            mTimePicker.show();
+//
+//            return true;
+//        });
+    }
+
+    private void setTime(EditText editText) {
+        Calendar mcurrentTime = Calendar.getInstance();
+        int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+        int minute = mcurrentTime.get(Calendar.MINUTE);
+        TimePickerDialog mTimePicker;
+        mTimePicker = new TimePickerDialog(CreateTeamActivity.this,
+            (timePicker, selectedHour, selectedMinute) -> {
+                String selectedMinuteText;
+
+                if (selectedMinute < 10) {
+                    selectedMinuteText = "0" + selectedMinute;
+                } else {
+                    selectedMinuteText = "" + selectedMinute;
+                }
+
+                editText.setText(selectedHour + ":" + selectedMinuteText);
+            },
+            hour,
+            minute,
+            true);
+        mTimePicker.setTitle("Select Time");
+        mTimePicker.show();
     }
 
     @OnClick(R.id.create_team_btn)
@@ -92,6 +141,27 @@ public class CreateTeamActivity extends AppCompatActivity {
         mDatabaseReference.child("team").child(key).setValue(team);
         mDatabaseReference.child("usersTeam").child(key).setValue(usersTeam);
     }
+
+//    @OnTouch(R.id.start_time_edt)
+//    public boolean setTime(View view) {
+//        EditText editText = (EditText) view;
+//
+//        Calendar mcurrentTime = Calendar.getInstance();
+//        int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+//        int minute = mcurrentTime.get(Calendar.MINUTE);
+//        TimePickerDialog mTimePicker;
+//        mTimePicker = new TimePickerDialog(CreateTeamActivity.this,
+//                (timePicker, selectedHour, selectedMinute) -> {
+//                    editText.setText( selectedHour + ":" + selectedMinute);
+//                },
+//                hour,
+//                minute,
+//                true);
+//        mTimePicker.setTitle("Select Time");
+//        mTimePicker.show();
+//
+//        return false;
+//    }
 
     private boolean validateForm() {
         boolean valid = true;
--- app/src/main/java/com/carlosvinicius/gingasoccer/RegisterActivity.java
+++ app/src/main/java/com/carlosvinicius/gingasoccer/RegisterActivity.java
@@ -1,5 +1,6 @@
 package com.carlosvinicius.gingasoccer;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,6 +8,7 @@ import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -14,6 +16,8 @@ import com.carlosvinicius.gingasoccer.models.User;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Calendar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -58,6 +62,41 @@ public class RegisterActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance();
         usersDatabaseReference = database.getReference("users");
+
+        birthDateEditText.setOnClickListener(view -> {
+            final Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+
+            DatePickerDialog datePickerDialog;
+
+            datePickerDialog = new DatePickerDialog(RegisterActivity.this,
+                (datePicker, selectedYear, selectedMonth, selectedDay) -> {
+                    String selectedMonthText;
+                    String selectedDayText;
+
+                    if (selectedMonth < 10) {
+                        selectedMonthText = "0" + selectedMonth;
+                    } else {
+                        selectedMonthText = "" + selectedMonth;
+                    }
+
+                    if (selectedDay < 10) {
+                        selectedDayText = "0" + selectedDay;
+                    } else {
+                        selectedDayText = "" + selectedDay;
+                    }
+
+                    birthDateEditText.setText(selectedDayText + "/" + selectedMonthText + "/" + selectedYear);
+                },
+                year,
+                month,
+                day);
+
+            datePickerDialog.setTitle("Select Date");
+            datePickerDialog.show();
+        });
     }
 
     @OnClick(R.id.sign_in_btn)
